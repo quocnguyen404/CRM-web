f424d44385ed53fecc637248260c29a6e5e4ec10
quocnguyen404 <125229853+quocnguyen404@users.noreply.github.com>
2023-04-07 10:18:38 +0000

    slide done

--- assest/javascript/main.js
+++ assest/javascript/main.js
@@ -1,17 +1,26 @@
-let slide_index = 0;
-let slide_img = Array(
-    "https://dq51jve9h21d4.cloudfront.net/sites/default/files/styles/article_thumbnail/public/news/1105/x060_a-v04c_b.png.pagespeed.ic.JH613iPMcl.webp",
-    "https://dq51jve9h21d4.cloudfront.net/sites/default/files/styles/article_thumbnail/public/news/1097/x125_a-v14-01i.png.pagespeed.ic.YUwqlmDe_N.webp",
-    "https://dq51jve9h21d4.cloudfront.net/sites/default/files/styles/article_thumbnail/public/crmland/217/xartboard-crmland-6.png.pagespeed.ic.uaQFYlK2b-.webp"
-)
+let slideIndex = 0;
+showSlides(slideIndex);
 
-function plus_Slide(j){
-    slide_index += j;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function change_slide(){
-    if (slide_index == -1){
-        slide_index = 2;
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide_img");
+
+    if (slideIndex < 0){
+        slideIndex = 2
     }
-    document.getElementById("slide_img").src = slide_img[slide_index];
+    if (slideIndex > slides.length - 1)
+    {
+        slideIndex = 0;
+    }
+
+    for(i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
 }
