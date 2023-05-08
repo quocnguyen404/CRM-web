3310124b6ad341109a2163116a6e92b2f7371a38
quocnguyen404 <quocnguyentdt@gmail.com>
2023-05-08 16:46:23 +0700

    complete

--- assest/javascript/main2.js
+++ assest/javascript/main2.js
@@ -1,9 +1,32 @@
+
+
+
 $(document).ready(function(){
 
     $(".advertising-container").click(function(){
         $("#advertising-tab").animate({
             width: 'toggle'
         })
+    })
+
+    $("#load-more-btn").click(function(){
+       $(".load-more-content1").fadeIn();
+
+        $("#load-more-btn").click(function(){
+            $(".load-more-content2").fadeIn();
+
+            $("#load-more-btn").click(function(){
+                $(".load-more-content3").fadeIn();
+            })
+        })
+    })
+
+    $("#sub-sologan > .sub-btn-area").click(function(){
+        $(".sub-input").focus();
+    })
+
+    $(".subscribe-btn").click(function(){
+        $(".sub-input").focus();
     })
 
 })
